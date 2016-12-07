f7f79bb389186f8aa10a7e796ead3b330d4f39f3
baivarn-tjr <t.sukkree@hotmail.com>
2016-12-07 05:03:25 -0500

    use array and for to shorten code

--- ThinkTwice.py
+++ ThinkTwice.py
@@ -15,7 +15,8 @@ rand_type = randint(0,1)
 rand_direction = randint(0,3)
 rand_color = randint(0,3)
 
-question = [rand_color,rand_direction]
+questionRnd = [rand_color,rand_direction]
+question = [0,0]
 
 class ThinkTwiceWindow(arcade.Window):
 	def __init__(self, width, height):
@@ -25,33 +26,26 @@ class ThinkTwiceWindow(arcade.Window):
 		
 		self.image = [["images/green.png", "images/red.png", "images/blue.png", "images/yellow.png"],["images/left.png", "images/down.png", "images/up.png", "images/right.png"]]
 
-		for i in range(0,2):
-			for j in range(0,4):
-				Arrow[i][j] = arcade.Sprite(self.image[i][j],scale)
-				
 		Arrow[2] = [230,370,370,510]
 		Arrow[3] = [130,130,235,130]
-
-		for i in range(0,4):
-			Arrow[0][i].set_position(Arrow[2][i],Arrow[3][i])
-			Arrow[1][i].set_position(Arrow[2][i],Arrow[3][i])
-
-		self.questionColor = arcade.Sprite(self.image[0][question[0]],scale)
-		self.questionDirection = arcade.Sprite(self.image[1][question[1]],scale)
-		self.questionColor.set_position(530,390)
-		self.questionDirection.set_position(530,390)
+		
+		for i in range(0,2):
+			question[i] = arcade.Sprite(self.image[i][questionRnd[i]],scale)
+			question[i].set_position(530,390)
+			for j in range(0,4):
+				Arrow[i][j] = arcade.Sprite(self.image[i][j],scale)
+				Arrow[i][j].set_position(Arrow[2][j],Arrow[3][j])
 
 	def on_draw(self):
 		arcade.start_render()
 		 
 		for i in range(0,2):
+			question[i].draw()
 			for j in range(0,4):
 				Arrow[i][j].draw()
 		
 		arcade.draw_text("Which one has",250,460,arcade.color.WHITE, 20)
 		arcade.draw_text("the same "+Type[rand_type]+" with",130,390,arcade.color.WHITE, 20)
-		self.questionColor.draw()
-		self.questionDirection.draw()
 
 if __name__ == '__main__':
 	window = ThinkTwiceWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
