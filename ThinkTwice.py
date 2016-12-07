4a93014d2173f1a1649b34ed38820998bd71f892
baivarn-tjr <t.sukkree@hotmail.com>
2016-12-07 04:05:46 -0500

    use array to shorten code

--- ThinkTwice.py
+++ ThinkTwice.py
@@ -1,12 +1,14 @@
 import arcade
+
+#from models import Arrow
  
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 scale = 0.6
 
-RED = (255, 0, 0)
- 
+Arrow = [[0 for x in range(4)] for y in range(4)]
+    
 class ThinkTwiceWindow(arcade.Window):
 	def __init__(self, width, height):
 		super().__init__(width, height)
@@ -14,40 +16,32 @@ class ThinkTwiceWindow(arcade.Window):
 		arcade.set_background_color(arcade.color.BLACK)
 		
 		self.leftArrow = arcade.Sprite('images/left.png',scale)
-		self.leftArrow.set_position(230,150)		
 		self.green = arcade.Sprite('images/green.png',scale)
-		self.green.set_position(230,150)
 
- 		
 		self.downArrow = arcade.Sprite('images/down.png',scale)
-		self.downArrow.set_position(370,150)		
 		self.red = arcade.Sprite('images/red.png',scale)
-		self.red.set_position(370,150)
 		
 		self.upArrow = arcade.Sprite('images/up.png',scale)
-		self.upArrow.set_position(370,255)		
 		self.blue = arcade.Sprite('images/blue.png',scale)
-		self.blue.set_position(370,255)
 	
 		self.rightArrow = arcade.Sprite('images/right.png',scale)
-		self.rightArrow.set_position(510,150)		
 		self.yellow = arcade.Sprite('images/yellow.png',scale)
-		self.yellow.set_position(510,150)
+
+		Arrow[0] = [self.green, self.red, self.blue, self.yellow]
+		Arrow[1] = [self.leftArrow, self.upArrow, self.downArrow, self.rightArrow]
+		Arrow[2] = [230,370,370,510]
+		Arrow[3] = [150,150,255,150]
+
+		for i in range(0,4):
+			Arrow[0][i].set_position(Arrow[2][i],Arrow[3][i])
+			Arrow[1][i].set_position(Arrow[2][i],Arrow[3][i])
 
 	def on_draw(self):
 		arcade.start_render()
 		 
-		self.green.draw()
-		self.leftArrow.draw()
-		
-		self.blue.draw()
-		self.upArrow.draw()
-
-		self.red.draw()
-		self.downArrow.draw()
-
-		self.yellow.draw()
-		self.rightArrow.draw()
+		for i in range(0,2):
+			for j in range(0,4):
+				Arrow[i][j].draw()
 
 if __name__ == '__main__':
 	window = ThinkTwiceWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
