95e54c379308a5c3a01c575e6e91ac78854b6950
baivarn-tjr <t.sukkree@hotmail.com>
2016-12-07 04:57:01 -0500

    can ask question

--- ThinkTwice.py
+++ ThinkTwice.py
@@ -1,6 +1,7 @@
 import arcade
 
 #from models import Arrow
+from random import randint
  
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -8,33 +9,37 @@ SCREEN_HEIGHT = 600
 scale = 0.6
 
 Arrow = [[0 for x in range(4)] for y in range(4)]
-    
+
+Type = ["direction","color"]
+rand_type = randint(0,1)
+rand_direction = randint(0,3)
+rand_color = randint(0,3)
+
+question = [rand_color,rand_direction]
+
 class ThinkTwiceWindow(arcade.Window):
 	def __init__(self, width, height):
 		super().__init__(width, height)
  
 		arcade.set_background_color(arcade.color.BLACK)
 		
-		self.leftArrow = arcade.Sprite('images/left.png',scale)
-		self.green = arcade.Sprite('images/green.png',scale)
+		self.image = [["images/green.png", "images/red.png", "images/blue.png", "images/yellow.png"],["images/left.png", "images/down.png", "images/up.png", "images/right.png"]]
 
-		self.downArrow = arcade.Sprite('images/down.png',scale)
-		self.red = arcade.Sprite('images/red.png',scale)
-		
-		self.upArrow = arcade.Sprite('images/up.png',scale)
-		self.blue = arcade.Sprite('images/blue.png',scale)
-	
-		self.rightArrow = arcade.Sprite('images/right.png',scale)
-		self.yellow = arcade.Sprite('images/yellow.png',scale)
-
-		Arrow[0] = [self.green, self.red, self.blue, self.yellow]
-		Arrow[1] = [self.leftArrow, self.upArrow, self.downArrow, self.rightArrow]
+		for i in range(0,2):
+			for j in range(0,4):
+				Arrow[i][j] = arcade.Sprite(self.image[i][j],scale)
+				
 		Arrow[2] = [230,370,370,510]
-		Arrow[3] = [150,150,255,150]
+		Arrow[3] = [130,130,235,130]
 
 		for i in range(0,4):
 			Arrow[0][i].set_position(Arrow[2][i],Arrow[3][i])
 			Arrow[1][i].set_position(Arrow[2][i],Arrow[3][i])
+
+		self.questionColor = arcade.Sprite(self.image[0][question[0]],scale)
+		self.questionDirection = arcade.Sprite(self.image[1][question[1]],scale)
+		self.questionColor.set_position(530,390)
+		self.questionDirection.set_position(530,390)
 
 	def on_draw(self):
 		arcade.start_render()
@@ -42,6 +47,11 @@ class ThinkTwiceWindow(arcade.Window):
 		for i in range(0,2):
 			for j in range(0,4):
 				Arrow[i][j].draw()
+		
+		arcade.draw_text("Which one has",250,460,arcade.color.WHITE, 20)
+		arcade.draw_text("the same "+Type[rand_type]+" with",130,390,arcade.color.WHITE, 20)
+		self.questionColor.draw()
+		self.questionDirection.draw()
 
 if __name__ == '__main__':
 	window = ThinkTwiceWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
